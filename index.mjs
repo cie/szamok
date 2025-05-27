d821ce7b7425166f69a539e1d2145ffd91007c07
TothBalazs06 <tothbalazs1902@gmail.com>
2025-05-27 17:25:13 +0200

    hibajavitas es tesztek

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 export function szam(n) {
   const szamok = {
-    30: 'harminc', 40: 'negyven', 50: 'ötven',
-    60: 'hatvan', 70: 'hetven', 80: 'nyolcvan', 90: 'kilencven',
     1: 'egy', 2: 'kettő', 3: 'három', 4: 'négy', 5: 'öt',
-    6: 'hat', 7: 'hét', 8: 'nyolc', 9: 'kilenc'
+    6: 'hat', 7: 'hét', 8: 'nyolc', 9: 'kilenc',
+    30: 'harminc', 40: 'negyven', 50: 'ötven',
+    60: 'hatvan', 70: 'hetven', 80: 'nyolcvan', 90: 'kilencven'
   };
 
   let tizes = Math.floor(n / 10) * 10;
--- test.mjs
+++ test.mjs
@@ -3,3 +3,8 @@ import { szam } from './index.mjs'
 console.log(1, szam(1))
 console.log(3, szam(3))
 console.log(10, szam(10))
+console.log(15, szam(15))
+console.log(30, szam(30))
+console.log(42, szam(42))
+console.log(75, szam(75))
+console.log(99, szam(99))

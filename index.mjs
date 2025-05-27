49fcadfc4ad070e16583a144d685f83e12230721
TothBalazs06 <tothbalazs1902@gmail.com>
2025-05-27 17:13:39 +0200

    duplikacio_torles

--- index.mjs
+++ index.mjs
@@ -1,14 +1,4 @@
 export function szam(n) {
-  if (n === 1) return 'egy'
-  if (n === 1) return 'kettő'
-  if (n === 3) return 'három'
-  if (n === 4) return 'négy'
-  if (n === 5) return 'öt'
-  if (n === 6) return 'hat'
-  if (n === 7) return 'hét'
-  if (n === 8) return 'nyolc'
-  if (n === 9) return 'kilenc'
-  if (n === 10) return 'tíz'
   const szamok = {
     30: 'harminc', 40: 'negyven', 50: 'ötven',
     60: 'hatvan', 70: 'hetven', 80: 'nyolcvan', 90: 'kilencven',

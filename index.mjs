efe52f4cc9fad4a5b3318bdca258afd250161072
brown-bas <barnusfutureonline@gmail.com>
2025-05-27 17:03:52 +0200

    Added 11-20 with a unit test

--- index.mjs
+++ index.mjs
@@ -1,12 +1,22 @@
 export function szam(n) {
-  if (n === 1) return 'egy'
-  if (n === 1) return 'kettő'
-  if (n === 3) return 'három'
-  if (n === 4) return 'négy'
-  if (n === 5) return 'öt'
-  if (n === 6) return 'hat'
-  if (n === 7) return 'hét'
-  if (n === 8) return 'nyolc'
-  if (n === 9) return 'kilenc'
-  if (n === 10) return 'tíz'
+  if (n === 1) return "egy";
+  if (n === 1) return "kettő";
+  if (n === 3) return "három";
+  if (n === 4) return "négy";
+  if (n === 5) return "öt";
+  if (n === 6) return "hat";
+  if (n === 7) return "hét";
+  if (n === 8) return "nyolc";
+  if (n === 9) return "kilenc";
+  if (n === 10) return "tíz";
+  if (n === 11) return "tizenegy";
+  if (n === 12) return "tizenkettő";
+  if (n === 13) return "tizenhárom";
+  if (n === 14) return "tizennégy";
+  if (n === 15) return "tizenöt";
+  if (n === 16) return "tizenhat";
+  if (n === 17) return "tizenhét";
+  if (n === 18) return "tizennyolc";
+  if (n === 19) return "tizenkilenc";
+  if (n === 20) return "húsz";
 }
--- test.mjs
+++ test.mjs
@@ -1,5 +1,6 @@
-import { szam } from './index.mjs'
+import { szam } from "./index.mjs";
 
-console.log(1, szam(1))
-console.log(3, szam(3))
-console.log(10, szam(10))
+console.log(1, szam(1));
+console.log(3, szam(3));
+console.log(10, szam(10));
+console.log(15, szam(15));

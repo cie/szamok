9b77b06c53663b4d509cf0f8b3bf865390fe3b38
Bernát Kalló <kallo.bernat@gmail.com>
2025-05-27 15:35:18 +0200

    Basic version 1-10

---
+++ index.js
@@ -0,0 +1,12 @@
+export function szam(n) {
+  if (n === 1) return 'egy'
+  if (n === 1) return 'kettő'
+  if (n === 3) return 'három'
+  if (n === 4) return 'négy'
+  if (n === 5) return 'öt'
+  if (n === 6) return 'hat'
+  if (n === 7) return 'hét'
+  if (n === 8) return 'nyolc'
+  if (n === 9) return 'kilenc'
+  if (n === 10) return 'tíz'
+}

d6eb8d07e6f2da0f663c803688c4d4f9ac9e1680
Levente <levente.szomor@gmail.com>
2025-05-27 17:31:48 +0200

    Teszteles atirasa

--- test.mjs
+++ test.mjs
@@ -2,6 +2,20 @@
 
 import { szam } from './index.mjs'
 
-console.log(1, szam(1))
-console.log(3, szam(3))
-console.log(10, szam(10))
+console.log(100, szam(100))
+console.log(101, szam(101))
+console.log(110, szam(110))
+console.log(123, szam(123))
+console.log(200, szam(200))
+console.log(212, szam(212))
+console.log(300, szam(300))
+console.log(321, szam(321))
+console.log(400, szam(400))
+console.log(500, szam(500))
+console.log(654, szam(654))
+console.log(700, szam(700))
+console.log(789, szam(789))
+console.log(800, szam(800))
+console.log(888, szam(888))
+console.log(900, szam(900))
+console.log(999, szam(999))

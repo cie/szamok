9b86f547ebbf426178741fafb6840193e402c39f
TothBalazs06 <tothbalazs1902@gmail.com>
2025-05-27 16:54:00 +0200

    30-99

--- index.mjs
+++ index.mjs
@@ -9,4 +9,17 @@ export function szam(n) {
   if (n === 8) return 'nyolc'
   if (n === 9) return 'kilenc'
   if (n === 10) return 'tíz'
+  const szamok = {
+    30: 'harminc', 40: 'negyven', 50: 'ötven',
+    60: 'hatvan', 70: 'hetven', 80: 'nyolcvan', 90: 'kilencven'
+  };
+
+  if (szamok[n]) {
+    return szamok[n];
+  }
+
+  let tizes = Math.floor(n / 10) * 10;
+  let egyes = n % 10;
+
+  return egyes === 0 ? szamok[tizes] : `${szamok[tizes]}${szamok[egyes]}`;
 }

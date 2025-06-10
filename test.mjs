5b3e860b24cd5e93ee24777ed636665dbff6324f
Szabó Dávid <szabodavid202@gmail.com>
2025-06-10 16:43:28 +0200

    test

--- test.mjs
+++ test.mjs
@@ -1,11 +1,19 @@
-import { szam } from './index.mjs'
+// Test cases
+console.log('Egész számok:');
+console.log(0, szam(0));
+console.log(1, szam(1));
+console.log(3, szam(3));
+console.log(8, szam(8));
+console.log(10, szam(10));
+console.log(15, szam(15));
+console.log(30, szam(30));
+console.log(42, szam(42));
+console.log(75, szam(75));
+console.log(99, szam(99));
 
-console.log(1, szam(1))
-console.log(3, szam(3))
-console.log(8, szam(8))
-console.log(10, szam(10))
-console.log(15, szam(15))
-console.log(30, szam(30))
-console.log(42, szam(42))
-console.log(75, szam(75))
-console.log(99, szam(99))
+console.log('\nTizedes számok:');
+console.log(1.5, szam(1.5));
+console.log(3.14, szam(3.14));
+console.log(0.5, szam(0.5));
+console.log(42.75, szam(42.75));
+console.log(0.123, szam(0.123));
